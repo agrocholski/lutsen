514db66e6e9c15107031e24e5493c797514a8332
adam grocholski <adam@thinkfirstcodelater.com>
2014-11-26 15:09:42 -0600

    AzureResourceMonitor class

---
+++ src/Lutsen.Models/AzureResourceMonitor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lutsen.Models
+{
+    public class AzureResourceMonitor
+    {
+        public AzureResourceMonitor()
+        {
+            Subscriptions = new List<AzureSubscription>();
+        }
+
+        public List<AzureSubscription> Subscriptions { get; set; }
+    }
+}
