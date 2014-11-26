f8ca90ff71a2ad23ea94428df3536bad2c1e1d68
adam grocholski <adam@thinkfirstcodelater.com>
2014-11-26 15:04:05 -0600

    AzureResource class

---
+++ src/Lutsen.Models/AzureResource.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.Management.Resources.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lutsen.Models
+{
+    public class AzureResource
+    {
+        public AzureResource()
+        {
+            Events = new List<AzureResourceEventData>();
+            MetricDefinitions = new List<AzureResourceMetricDefinition>();
+            Metrics = new List<AzureResourceMetric>();
+            UsageMetrics = new List<AzureResourceUsageMetric>();
+        }
+
+        public AzureResource(Resource resource, string subscriptionId, string resourceGroupName)
+            : this()
+        {
+            Id = resource.Id;
+            Name = resource.Name;
+            Type = resource.Type;
+            Uri = string.Format("subscriptions/{0}/resourceGroups/{1}/providers/{2}/{3}", subscriptionId, resourceGroupName, resource.Type, resource.Name);
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Uri { get; set; }
+        public List<AzureResourceEventData> Events { get; set; }
+        public List<AzureResourceMetricDefinition> MetricDefinitions { get; set; }
+        public List<AzureResourceMetric> Metrics { get; set; }
+        public List<AzureResourceUsageMetric> UsageMetrics { get; set; }
+
+    }
+}
