f2e098d58a357a247993bdb5a82f45586b32188e
adam grocholski <adam@thinkfirstcodelater.com>
2014-11-26 14:58:07 -0600

    initial project setup

---
+++ src/Lutsen.Models/AzureResourceUsageMetric.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.Insights.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lutsen.Models
+{
+    public class AzureResourceUsageMetric
+    {
+        public AzureResourceUsageMetric() { }
+
+        public AzureResourceUsageMetric(UsageMetric metric)
+        {
+            CurrentValue = metric.CurrentValue;
+            Limit = metric.Limit;
+            Name = metric.Name.Value;
+            NextResetTime = metric.NextResetTime;
+            QuotaPeriod = metric.QuotaPeriod;
+            Unit = metric.Unit;
+        }
+
+        public double CurrentValue { get; set; }
+        public double Limit { get; set; }
+        public string Name { get; set; }
+        public string NextResetTime { get; set; }
+        public TimeSpan? QuotaPeriod { get; set; }
+        public string Unit { get; set; }
+    }
+}
