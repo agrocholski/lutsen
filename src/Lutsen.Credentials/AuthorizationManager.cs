e22b1e82e70799cddf61e5c4757402a304b3b998
adam grocholski <adam@thinkfirstcodelater.com>
2014-11-26 15:28:45 -0600

    AuthorizationManager class

---
+++ src/Lutsen.Credentials/AuthorizationManager.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lutsen.Credentials
+{
+    public class AuthorizationManager
+    {
+        public static string GetAuthorizationToken(string tenantId, string clientId, string redirectUri)
+        {
+            AuthenticationResult result = null;
+
+            var context = new AuthenticationContext(string.Format("https://login.windows.net/{0}", tenantId));
+
+            var thread = new Thread(() =>
+            {
+                result = context.AcquireToken(
+                  "https://management.core.windows.net/",
+                  clientId,
+                  new Uri(redirectUri));
+            });
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Name = "AquireTokenThread";
+            thread.Start();
+            thread.Join();
+
+            if (result == null)
+            {
+                throw new InvalidOperationException("Failed to obtain the JWT token");
+            }
+
+            string token = result.AccessToken;
+
+            return token;
+        }
+    }
+}
