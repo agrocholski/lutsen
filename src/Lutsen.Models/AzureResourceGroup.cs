daad213d0296960de5b06fe9b085b23fc8263d5a
adam grocholski <adam@thinkfirstcodelater.com>
2014-11-26 15:05:10 -0600

    AzureResourceGroup class

---
+++ src/Lutsen.Models/AzureResourceGroup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Management.Resources.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lutsen.Models
+{
+    public class AzureResourceGroup
+    {
+        public AzureResourceGroup()
+        {
+            Resources = new List<AzureResource>();
+        }
+
+        public AzureResourceGroup(ResourceGroup resourceGroup)
+            : this()
+        {
+            Id = resourceGroup.Id;
+            Name = resourceGroup.Name;
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<AzureResource> Resources { get; set; }
+    }
+}
