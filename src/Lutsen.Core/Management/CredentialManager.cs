0a460cfa461c01c6fe2344b937a8e11f44eacc45
adam grocholski <adam@thinkfirstcodelater.com>
2014-12-02 18:26:47 -0600

    SubscriptionManager class

--- src/Lutsen.Core/Management/CredentialManager.cs
+++ src/Lutsen.Core/Management/CredentialManager.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lutsen.Credentials
+namespace Lutsen.Core.Management
 {
     public class CredentialManager
     {
---
+++ src/Lutsen.Core/Management/SubscriptionManager.cs
@@ -0,0 +1,37 @@
+﻿using Lutsen.Core.Models;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Subscriptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lutsen.Core.Management
+{
+    public class SubscriptionManager
+    {
+        public static List<AzureSubscription> GetSubscriptions(string token)
+        {
+            var credentials = CredentialManager.GetCredentials(token);
+
+            return GetSubscriptions(credentials);
+        }
+
+        public static List<AzureSubscription> GetSubscriptions(SubscriptionCloudCredentials credentials)
+        {
+            var result = new List<AzureSubscription>();
+
+            SubscriptionClient client = new SubscriptionClient(credentials);
+
+            var subscriptions = client.Subscriptions.List();
+
+            foreach (var subscription in subscriptions)
+            {
+                result.Add(new AzureSubscription(subscription));
+            }
+
+            return result;
+        }
+    }
+}
