6cb3f93900feb15f6d3a893dfd483810730bc7a5
adam grocholski <adam@thinkfirstcodelater.com>
2014-12-02 20:02:31 -0600

    capture params and retrieve auth token in console sample

--- src/Lutsen.Console/Program.cs
+++ src/Lutsen.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lutsen.Core.Management;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,26 @@ namespace Lutsen.Console
     {
         static void Main(string[] args)
         {
+            /* In order to run this sample application you must have an Azure Active Directory (AAD) tenant,
+             * and you must configure an application within the AAD tenant. For instructions on how to do so,
+             * please read the following article: http://msdn.microsoft.com/en-us/library/azure/dn790557.aspx
+             * */
+
+            System.Console.Write("Enter your AAD tenant id: ");
+            var tenantId = System.Console.ReadLine();
+
+            System.Console.Write("Enter your AAD application id: ");
+            var appId = System.Console.ReadLine();
+
+            System.Console.Write("Enter your AAD application redirect Uri: ");
+            var appRedirectUri = System.Console.ReadLine();
+
+            var authToken = AuthorizationManager.GetAuthorizationToken(tenantId, appId, appRedirectUri);
+
+            //todo: make magic happen
+
+            System.Console.WriteLine("Press any key to exit...");
+            System.Console.ReadLine();
         }
     }
 }
