15ddafd153c87a6c6bf39cf715dc90ca083b5fce
adam grocholski <adam@thinkfirstcodelater.com>
2014-12-02 19:08:08 -0600

    added ResourceGroupName prop to AzureResource class

--- src/Lutsen.Core/Models/AzureResource.cs
+++ src/Lutsen.Core/Models/AzureResource.cs
@@ -19,13 +19,15 @@ namespace Lutsen.Core.Models
         {
             Id = resource.Id;
             Name = resource.Name;
-            Type = resource.Type;
+            ResourceGroupName = ResourceGroupName;
             SubscriptionId = subscriptionId;
+            Type = resource.Type;
             Uri = string.Format("subscriptions/{0}/resourceGroups/{1}/providers/{2}/{3}", subscriptionId, resourceGroupName, resource.Type, resource.Name);
         }
 
         public string Id { get; set; }
         public string Name { get; set; }
+        public string ResourceGroupName { get; set; }
         public string SubscriptionId { get; set; }
         public string Type { get; set; }
         public string Uri { get; set; }
