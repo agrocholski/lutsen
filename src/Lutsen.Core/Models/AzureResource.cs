7b5e0b39f04fa83ffaf32bb35ecee06812fa6dda
adam grocholski <adam@thinkfirstcodelater.com>
2014-12-02 19:05:34 -0600

    added SubscriptionId prop to AzureResource

--- src/Lutsen.Core/Models/AzureResource.cs
+++ src/Lutsen.Core/Models/AzureResource.cs
@@ -20,11 +20,13 @@ namespace Lutsen.Core.Models
             Id = resource.Id;
             Name = resource.Name;
             Type = resource.Type;
+            SubscriptionId = subscriptionId;
             Uri = string.Format("subscriptions/{0}/resourceGroups/{1}/providers/{2}/{3}", subscriptionId, resourceGroupName, resource.Type, resource.Name);
         }
 
         public string Id { get; set; }
         public string Name { get; set; }
+        public string SubscriptionId { get; set; }
         public string Type { get; set; }
         public string Uri { get; set; }
         public AzureResourceTelemetry Telemetry { get; set; }
