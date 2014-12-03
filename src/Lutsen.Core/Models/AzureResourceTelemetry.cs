d4040c311576a96a3423f6ad51f58f98a040e6a2
adam grocholski <adam@thinkfirstcodelater.com>
2014-12-02 18:55:11 -0600

    AzureResourceTelemetry class

---
+++ src/Lutsen.Core/Models/AzureResourceTelemetry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lutsen.Core.Models
+{
+    public class AzureResourceTelemetry
+    {
+        public AzureResourceTelemetry()
+        {
+            Events = new List<AzureResourceEventData>();
+            MetricDefinitions = new List<AzureResourceMetricDefinition>();
+            Metrics = new List<AzureResourceMetric>();
+            UsageMetrics = new List<AzureResourceUsageMetric>();
+        }
+
+        public List<AzureResourceEventData> Events { get; set; }
+        public List<AzureResourceMetricDefinition> MetricDefinitions { get; set; }
+        public List<AzureResourceMetric> Metrics { get; set; }
+        public List<AzureResourceUsageMetric> UsageMetrics { get; set; }
+    }
+}
